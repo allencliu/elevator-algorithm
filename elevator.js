3ad81a8641fedc1f1403f358e1c38cf5398b7469
liuac <liuac@ceclnx01-19.csi.miamioh.edu>
2022-04-21 22:16:23 -0400

    Elevator 1 is working now (minor bug: starting floor 1, won't turn blue on pickup)

--- elevator.js
+++ elevator.js
@@ -1,6 +1,60 @@
 $( document ).ready(function() {
-	var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
 	$(".floor1").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor2").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor3").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor4").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor5").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor6").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor7").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor8").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor9").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
+		var dest = $(this).val();
+		var startFloor = this.getAttribute("data-floor");
+		pickUp(startFloor, dest, elevCurFloor);
+	});
+	$(".floor10").click(function() {
+		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
 		var dest = $(this).val();
 		var startFloor = this.getAttribute("data-floor");
 		pickUp(startFloor, dest, elevCurFloor);
@@ -10,34 +64,117 @@ const sleep = (ms) =>
 	new Promise(resolve => setTimeout(resolve, ms));
 
 // rename to moveUp fucntion later
-async function moving(startFloor, dest, elevCurFloor) {
-	let nextFloor = startFloor + 1;
+async function moveUpDropOff(startFloor, dest, elevCurFloor) {
+	let nextFloor = elevCurFloor + 1;
 	//console.log(elevCurFloor);
-	if (startFloor < dest) {
+	if (elevCurFloor < dest) {
 		$("#e1f" + nextFloor).css("background", "green");
-		$("#e1f" + startFloor).removeClass("curFloor1");
+		$("#e1f" + elevCurFloor).removeClass("curFloor1");
 		$("#e1f" + nextFloor).addClass("curFloor1");
-		$("#e1f"+startFloor).css("background", "grey");
-		elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor");
-		console.log(elevCurFloor);
+		$("#e1f" + elevCurFloor).css("background", "grey");
+		//console.log(elevCurFloor);
 		await sleep(1000);
 		//console.log(curFloor);
 		// test to see if moving elevator class works
 		//console.log($("#e1f3").hasClass("curFloor1"));
-		startFloor+=1;
-		setTimeout(moving(Number(startFloor),Number(dest),Number(elevCurFloor)));
+		elevCurFloor+=1;
+		setTimeout(moveUpDropOff(Number(startFloor),Number(dest),Number(elevCurFloor)));
 	}
-	if (startFloor == dest){
+	if (elevCurFloor == dest){
 		setTimeout(arrived(dest), 1000);
 	}
 }
 
+async function moveDownDropOff(startFloor, dest, elevCurFloor) {
+	let nextFloor = elevCurFloor - 1;
+	//console.log(elevCurFloor);
+	if (elevCurFloor > dest) {
+		$("#e1f" + nextFloor).css("background", "green");
+		$("#e1f" + elevCurFloor).removeClass("curFloor1");
+		$("#e1f" + nextFloor).addClass("curFloor1");
+		$("#e1f" + elevCurFloor).css("background", "grey");
+		//console.log(elevCurFloor);
+		await sleep(1000);
+		//console.log(curFloor);
+		// test to see if moving elevator class works
+		//console.log($("#e1f3").hasClass("curFloor1"));
+		elevCurFloor -= 1;
+		setTimeout(moveDownDropOff(Number(startFloor),Number(dest),Number(elevCurFloor)));
+	}
+	if (elevCurFloor == dest){
+		setTimeout(arrived(dest), 1000);
+	}
+}
+
+
+async function moveUpPickUp(startFloor, dest, elevCurFloor) {
+	let nextFloor = elevCurFloor + 1;
+	//console.log(elevCurFloor);
+	if (elevCurFloor < startFloor) {
+		$("#e1f" + nextFloor).css("background", "green");
+		$("#e1f" + elevCurFloor).removeClass("curFloor1");
+		$("#e1f" + nextFloor).addClass("curFloor1");
+		$("#e1f" + elevCurFloor).css("background", "grey");
+		//console.log(elevCurFloor);
+		await sleep(1000);
+		//console.log(curFloor);
+		// test to see if moving elevator class works
+		//console.log($("#e1f3").hasClass("curFloor1"));
+		//startFloor+=1;
+		elevCurFloor += 1;
+		setTimeout(moveUpPickUp(Number(startFloor), Number(dest),  Number(elevCurFloor)));
+	}
+	if (elevCurFloor == startFloor){
+		setTimeout(arrived(startFloor), 1000);
+		pickUp(startFloor, dest, elevCurFloor);
+	}
+}
+
+async function moveDownPickUp(startFloor, dest, elevCurFloor) {
+	let nextFloor = elevCurFloor - 1;
+	//console.log(elevCurFloor);
+	if (elevCurFloor > startFloor) {
+		$("#e1f" + nextFloor).css("background", "green");
+		$("#e1f" + elevCurFloor).removeClass("curFloor1");
+		$("#e1f" + nextFloor).addClass("curFloor1");
+		$("#e1f" + elevCurFloor).css("background", "grey");
+		//console.log(elevCurFloor);
+		await sleep(1000);
+		//console.log(curFloor);
+		// test to see if moving elevator class works
+		//console.log($("#e1f3").hasClass("curFloor1"));
+		//startFloor+=1;
+		elevCurFloor -= 1;
+		setTimeout(moveDownPickUp(Number(startFloor), Number(dest),  Number(elevCurFloor)));
+	}
+	if (elevCurFloor == startFloor){
+		setTimeout(arrived(startFloor), 1000);
+		pickUp(startFloor, dest, elevCurFloor);
+	}
+}
+
+
 // need if statement to differentiate moveUp and moveDown
 function pickUp(startFloor, dest, elevCurFloor) {
-	$("#e1f"+startFloor).css("background", "blue");
-	setTimeout(function(){$("#e1f"+startFloor).css("background", "grey");
-		setTimeout(moving(Number(startFloor), Number(dest), Number(elevCurFloor)))}, 1000);
+	// move up
+	//console.log("startFloor: " + startFloor + " elevCurFloor: " + elevCurFloor);
+	if (startFloor > elevCurFloor) {
+		moveUpPickUp(Number(startFloor), Number(dest),  Number(elevCurFloor));
+	} else if (startFloor < elevCurFloor) { // move down 
+		moveDownPickUp(Number(startFloor), Number(dest), Number(elevCurFloor));
+	} else {
+		// pick up
+		if (startFloor < dest) {
+			$("#e1f"+startFloor).css("background", "blue");
+			setTimeout(function(){$("#e1f"+startFloor).css("background", "grey");
+				setTimeout(moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor)))}, 1000);
 
+		} else if (startFloor > dest) {
+			$("#e1f"+startFloor).css("background", "blue");
+			setTimeout(function(){$("#e1f"+startFloor).css("background", "grey");
+				setTimeout(moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor)))}, 1000);
+		}
+	}
 }
 
 function arrived(dest) {
