094572f0af0c323c3357b6dcbf2ff9a2e671e3e0
liuac <liuac@ceclnx01-19.csi.miamioh.edu>
2022-05-02 17:36:49 -0400

    Added elevator class

--- elevator.js
+++ elevator.js
@@ -46,11 +46,158 @@ class Queue {
 	}
 }
 
+const sleep = (ms) =>
+	new Promise(resolve => setTimeout(resolve, ms));
+class Elevator {
+	constructor(curFloorStr) {
+		this.elevatorFull = false;
+		this.curFloorStr = curFloorStr;
+	}
+	async pickUp() {
+		if (!this.elevatorFull && queue.peek() != null) {
+			var p = queue.poll();
+			var startFloor = p.getStartFloor();
+			var dest = p.getDest();
+			var elevCurFloor = $("." + this.curFloorStr)[0].getAttribute("data-curFloor")
+			var elevID = $("." + this.curFloorStr)[0].getAttribute("data-id")
+			this.elevatorFull = true;
+			if (startFloor > elevCurFloor) {
+				this.moveUpPickUp(Number(startFloor), Number(dest),  Number(elevCurFloor), elevID);
+			} else if (startFloor < elevCurFloor) { 
+				this.moveDownPickUp(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
+			} else {
+				if (startFloor < dest) {
+					$("#" + elevID + startFloor).css("background", "blue");
+					await sleep(1000)
+					$("#" + elevID + startFloor).css("background", "grey");
+					this.moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
+
+				} else if (startFloor > dest) {
+					$("#" + elevID + startFloor).css("background", "blue");
+					await sleep(1000);
+					$("#" + elevID + startFloor).css("background", "grey");
+					this.moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
+				}
+			}
+		} 
+	}
+
+	async moveUpPickUp(startFloor, dest, elevCurFloor, elevID) {
+		while (elevCurFloor < startFloor) {
+			console.log(queue);
+			let nextFloor = elevCurFloor + 1;
+			$("#" + elevID +  nextFloor).css("background", "green");
+			$("#" + elevID + elevCurFloor).removeClass("curFloor1");
+			$("#" + elevID + nextFloor).addClass("curFloor1");
+			$("#" + elevID + elevCurFloor).css("background", "grey");
+			await sleep(1000);
+			elevCurFloor += 1;
+		}
+		if (elevCurFloor == startFloor){
+			if (startFloor < dest) {
+				$("#" + elevID + startFloor).css("background", "blue");
+				await sleep(1000)
+				$("#" + elevID + startFloor).css("background", "grey");
+				this.moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
+
+			} else if (startFloor > dest) {
+				$("#" + elevID + startFloor).css("background", "blue");
+				await sleep(1000);
+				$("#" + elevID + startFloor).css("background", "grey");
+				this.moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
+			}
+		}
+	}
+
+
+	async moveDownPickUp(startFloor, dest, elevCurFloor, elevID) {
+		while (elevCurFloor > startFloor) {
+			let nextFloor = elevCurFloor - 1;
+			$("#" + elevID + nextFloor).css("background", "green");
+			$("#" + elevID + elevCurFloor).removeClass("curFloor1");
+			$("#" + elevID + nextFloor).addClass("curFloor1");
+			$("#" + elevID + elevCurFloor).css("background", "grey");
+			await sleep(1000);
+			elevCurFloor -= 1;
+		}
+		if (elevCurFloor == startFloor){
+			if (startFloor < dest) {
+				$("#" + elevID + startFloor).css("background", "blue");
+				await sleep(1000)
+				$("#" + elevID + startFloor).css("background", "grey");
+				this.moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
+
+			} else if (startFloor > dest) {
+				$("#" + elevID + startFloor).css("background", "blue");
+				await sleep(1000);
+				$("#" + elevID + startFloor).css("background", "grey");
+				this.moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
+			}
+		}
+	}
+
+	async moveUpDropOff(startFloor, dest, elevCurFloor, elevID) {
+		while (elevCurFloor < dest) {
+			let nextFloor = elevCurFloor + 1;
+			$("#" + elevID + nextFloor).css("background", "green");
+			$("#" + elevID + elevCurFloor).removeClass("curFloor1");
+			$("#" + elevID + nextFloor).addClass("curFloor1");
+			$("#" + elevID + elevCurFloor).css("background", "grey");
+			await sleep(1000);
+			elevCurFloor+=1;
+		}
+		if (elevCurFloor == dest){
+			this.arrived(dest, elevID);
+		}
+	}
+
+	async moveDownDropOff(startFloor, dest, elevCurFloor, elevID) {
+		while (elevCurFloor > dest) {
+			let nextFloor = elevCurFloor - 1;
+			$("#" + elevID + nextFloor).css("background", "green");
+			$("#" + elevID + elevCurFloor).removeClass("curFloor1");
+			$("#" + elevID + nextFloor).addClass("curFloor1");
+			$("#" + elevID + elevCurFloor).css("background", "grey");
+			await sleep(1000);
+			elevCurFloor -= 1;
+		}
+		if (elevCurFloor == dest){
+			this.arrived(dest, elevID)
+		}
+	}
+	async arrived(dest, elevID) {
+		if (elevID == "e1f") {
+			$("#" + elevID + dest).css("background", "black");
+			this.elevatorFull = false;
+			if (!this.elevatorFull && queue.peek() != null) {
+				await sleep(1000);
+				this.pickUp();
+			}
+		} else if (elevID == "e2f") {
+			$("#" + elevID + dest).css("background", "black");
+			this.elevator = false;
+			if (!this.elevatorFull && queue.peek() != null) {
+				await sleep(1000);
+				this.pickUp();
+			}
+		} else if (elevID == "e3f") {
+			$("#" + elevID + dest).css("background", "black");
+			this.elevator = false;
+			if (!this.elevatorFull && queue.peek() != null) {
+				await sleep(1000);
+				this.pickUp();
+			}
+		}
+	}
+	isFull() {
+		return this.elevatorFull;
+	}
+}
 
 var queue = new Queue();
-var elevator1Full = false;
-var elevator2Full = false;
-var elevator3Full = false;
+var elevator1 = new Elevator("curFloor1");
+var elevator2 = new Elevator("curFloor2");
+var elevator3 = new Elevator("curFloor3");
 $( document ).ready(function() {
 	$(".floor1").unbind('click').click(function() {
 		var dest = $(this).val();
@@ -124,211 +271,18 @@ $( document ).ready(function() {
 	});
 
 	$(".btn").click(function() {
-		if (!elevator1Full && queue.peek() != null) {
-			pickUp();
-		} else if (!elevator2Full && queue.peek() != null) {
-			pickUp();
-		} else if (!elevator3Full && queue.peek() != null) {	
-			pickUp();
-		}
+		if (!elevator1.isFull() && queue.peek() != null) {
+			elevator1.pickUp();
+		} /*else if (!elevator2.isFull() && queue.peek() != null) {
+			elevator2.pickUp();
+		} else if (!elevator3.isFull() && queue.peek() != null) {	
+			elevator3.pickUp();
+		}*/
 	});
 
 });
-const sleep = (ms) =>
-	new Promise(resolve => setTimeout(resolve, ms));
-
-async function moveUpDropOff(startFloor, dest, elevCurFloor, elevID) {
-	while (elevCurFloor < dest) {
-		let nextFloor = elevCurFloor + 1;
-		$("#" + elevID + nextFloor).css("background", "green");
-		$("#" + elevID + elevCurFloor).removeClass("curFloor1");
-		$("#" + elevID + nextFloor).addClass("curFloor1");
-		$("#" + elevID + elevCurFloor).css("background", "grey");
-		await sleep(1000);
-		elevCurFloor+=1;
-	}
-	if (elevCurFloor == dest){
-		arrived(dest, elevID);
-	}
-}
-
-async function moveDownDropOff(startFloor, dest, elevCurFloor, elevID) {
-	while (elevCurFloor > dest) {
-		let nextFloor = elevCurFloor - 1;
-		$("#" + elevID + nextFloor).css("background", "green");
-		$("#" + elevID + elevCurFloor).removeClass("curFloor1");
-		$("#" + elevID + nextFloor).addClass("curFloor1");
-		$("#" + elevID + elevCurFloor).css("background", "grey");
-		await sleep(1000);
-		elevCurFloor -= 1;
-	}
-	if (elevCurFloor == dest){
-		arrived(dest, elevID)
-	}
-}
-
-async function moveUpPickUp(startFloor, dest, elevCurFloor, elevID) {
-	while (elevCurFloor < startFloor) {
-		console.log(queue);
-		let nextFloor = elevCurFloor + 1;
-		$("#" + elevID +  nextFloor).css("background", "green");
-		$("#" + elevID + elevCurFloor).removeClass("curFloor1");
-		$("#" + elevID + nextFloor).addClass("curFloor1");
-		$("#" + elevID + elevCurFloor).css("background", "grey");
-		await sleep(1000);
-		elevCurFloor += 1;
-	}
-	if (elevCurFloor == startFloor){
-		if (startFloor < dest) {
-			$("#" + elevID + startFloor).css("background", "blue");
-			await sleep(1000)
-			$("#" + elevID + startFloor).css("background", "grey");
-			moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-
-		} else if (startFloor > dest) {
-			$("#" + elevID + startFloor).css("background", "blue");
-			await sleep(1000);
-			$("#" + elevID + startFloor).css("background", "grey");
-			moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
-		}
-	}
-}
 
 
-async function moveDownPickUp(startFloor, dest, elevCurFloor, elevID) {
-	while (elevCurFloor > startFloor) {
-		let nextFloor = elevCurFloor - 1;
-		$("#" + elevID + nextFloor).css("background", "green");
-		$("#" + elevID + elevCurFloor).removeClass("curFloor1");
-		$("#" + elevID + nextFloor).addClass("curFloor1");
-		$("#" + elevID + elevCurFloor).css("background", "grey");
-		await sleep(1000);
-		elevCurFloor -= 1;
-	}
-	if (elevCurFloor == startFloor){
-		if (startFloor < dest) {
-			$("#" + elevID + startFloor).css("background", "blue");
-			await sleep(1000)
-			$("#" + elevID + startFloor).css("background", "grey");
-			moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-
-		} else if (startFloor > dest) {
-			$("#" + elevID + startFloor).css("background", "blue");
-			await sleep(1000);
-			$("#" + elevID + startFloor).css("background", "grey");
-			moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
-		}
-	}
-}
-
-
-async function pickUp() {
-	if (!elevator1Full && queue.peek() != null) {
-		var p = queue.poll();
-		var startFloor = p.getStartFloor();
-		var dest = p.getDest();
-		var elevCurFloor = $(".curFloor1")[0].getAttribute("data-curFloor")
-		var elevID = $(".curFloor1")[0].getAttribute("data-id")
-		elevator1Full = true;
-		if (startFloor > elevCurFloor) {
-			moveUpPickUp(Number(startFloor), Number(dest),  Number(elevCurFloor), elevID);
-		} else if (startFloor < elevCurFloor) { 
-			moveDownPickUp(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-		} else {
-			if (startFloor < dest) {
-				$("#" + elevID + startFloor).css("background", "blue");
-				await sleep(1000)
-				$("#" + elevID + startFloor).css("background", "grey");
-				moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-
-			} else if (startFloor > dest) {
-				$("#" + elevID + startFloor).css("background", "blue");
-				await sleep(1000);
-				$("#" + elevID + startFloor).css("background", "grey");
-				moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
-			}
-		}
-	} else if (!elevator2Full && queue.peek() != null) {
-		var p = queue.poll();
-		var startFloor = p.getStartFloor();
-		var dest = p.getDest();
-		var elevCurFloor = $(".curFloor2")[0].getAttribute("data-curFloor")
-		var elevID = $(".curFloor2")[0].getAttribute("data-id")
-		elevator2Full = true;
-		if (startFloor > elevCurFloor) {
-			moveUpPickUp(Number(startFloor), Number(dest),  Number(elevCurFloor), elevID);
-		} else if (startFloor < elevCurFloor) { 
-			moveDownPickUp(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-		} else {
-			if (startFloor < dest) {
-				$("#" + elevID + startFloor).css("background", "blue");
-				await sleep(1000)
-				$("#" + elevID + startFloor).css("background", "grey");
-				moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-
-			} else if (startFloor > dest) {
-				$("#" + elevID + startFloor).css("background", "blue");
-				await sleep(1000);
-				$("#" + elevID + startFloor).css("background", "grey");
-				moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
-			}
-		}
-	} else if (!elevator3Full && queue.peek() != null) {
-		var p = queue.poll();
-		var startFloor = p.getStartFloor();
-		var dest = p.getDest();
-		var elevCurFloor = $(".curFloor3")[0].getAttribute("data-curFloor")
-		var elevID = $(".curFloor3")[0].getAttribute("data-id")
-		elevator3Full = true;
-		if (startFloor > elevCurFloor) {
-			moveUpPickUp(Number(startFloor), Number(dest),  Number(elevCurFloor), elevID);
-		} else if (startFloor < elevCurFloor) { 
-			moveDownPickUp(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-		}
-		else {
-			if (startFloor < dest) {
-				$("#" + elevID + startFloor).css("background", "blue");
-				await sleep(1000)
-				$("#" + elevID + startFloor).css("background", "grey");
-				moveUpDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID);
-
-			} else if (startFloor > dest) {
-				$("#" + elevID + startFloor).css("background", "blue");
-				await sleep(1000);
-				$("#" + elevID + startFloor).css("background", "grey");
-				moveDownDropOff(Number(startFloor), Number(dest), Number(elevCurFloor), elevID)
-			}
-		}
-
-	}
-}
-
-async function arrived(dest, elevID) {
-	if (elevID === "e1f") {
-		$("#" + elevID + dest).css("background", "black");
-		elevator1Full = false;
-		if (!elevator1Full && queue.peek() != null) {
-			await sleep(1000);
-			pickUp();
-		}
-	} else if (elevID === "e2f") {
-		$("#" + elevID + dest).css("background", "black");
-		elevator2Full = false;
-		await sleep(1000);
-		if (!elevator2Full && queue.peek() != null) {
-			pickUp();
-		}
-	} else if (elevID === "e3f") {
-		$("#" + elevID + dest).css("background", "black");
-		elevator3Full = false;
-		await sleep(1000);
-		if (!elevator3Full && queue.peek() != null) {
-			pickUp();
-		}
-
-	}
-
-}
 
 
 
